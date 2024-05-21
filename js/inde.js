17aaea37a882e36fbfaaca4ad3232061c8824003
hadziukk <127852832+hadziukk@users.noreply.github.com>
2024-05-21 12:09:40 +0000

    com 2

--- js/inde.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-        $( "#effect" ).toggle( selectedEffect, options, 500 );
-        $( "#effect2" ).toggle( selectedEffect, options, 500 );
-        $( "#effect3" ).toggle( selectedEffect, options, 500 );
-
-  });
--- js/zlota.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $("#effect").hide();
-  $("#effect2").hide();
-  $("#podium").hide();
-
-  $("#podium").slideDown(2000);
-});
